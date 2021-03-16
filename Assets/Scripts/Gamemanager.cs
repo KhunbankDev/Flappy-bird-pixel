d26dcb0b314f192723b5c9330506c3823b66d503
khunbank <khunbankkum@gmail.com>
2021-03-16 15:50:58 +0700

    recommit

---
+++ Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    public GameObject ModalGameOver,ModalPause,BtnPause;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+   public void GameOver(){
+       ModalGameOver.SetActive(true);
+       BtnPause.SetActive(false);
+       Time.timeScale = 0;
+   }
+
+   public void GamePause(){
+       ModalPause.SetActive(true);
+       Time.timeScale = 0;
+   }
+
+    public void GameResume(){
+        ModalPause.SetActive(false);
+       Time.timeScale = 1;
+   }
+
+   public void GameReplay(){
+       ManageScene loadScene = new ManageScene();
+       loadScene.LoadSceneIndex(1);
+   }
+}
---
+++ Assets/Scripts/PipeMoveLeft.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeMoveLeft : MonoBehaviour
+{
+  public float speedVal;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * speedVal * Time.deltaTime;
+    }
+}
